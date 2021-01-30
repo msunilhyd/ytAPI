5586f6c6c7cc5394f7eced465091f76fc191f01f
msunilhyd <msunilhyd@gmail.com>
2021-01-30 21:15:56 +0530

    30-Jan-21 : Sat : Added support for league objects

---
+++ classesPrototype.py
@@ -0,0 +1,14 @@
+class Employee:
+    pass
+
+emp_1 = Employee()
+emp_2 = Employee()
+
+emp_1.first = 'Corey'
+emp_2.last = 'Scafer'
+emp_1.email = 'msunilhyd@gmail.com'
+emp_1.pay = 49999
+
+
+print(emp_1.first)
+print(emp_2)
--- scrape.py
+++ scrape.py
@@ -1,37 +1,71 @@
 import requests
-page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
+#page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
+# print(page.content)
+import datetime
+
+curDate = datetime.datetime.utcnow().isoformat() + "Z"
+print(curDate)
+splittedDate = curDate.split('T')
+print(splittedDate[0])
+
+actual_url = "https://www.sportinglife.com/football/fixtures"
+page = requests.get(actual_url)
 # print(page.content)
 
 from bs4 import BeautifulSoup
-soup = BeautifulSoup(page.content, 'html.parser')
+# soup = BeautifulSoup(page.content, 'html.parser')
 # print(list(soup.children))
 
 # print([type(item) for item in list(soup.children)])
 
-html = list(soup.children)[2]
+# html = list(soup.children)[2]
 # print(list(html.children))
-body = list(html.children)[3]
-p = list(body.children)[1]
+# body = list(html.children)[3]
+# p = list(body.children)[1]
 # print(p.get_text())
 
+print("Hello World")
+# f = open("/home/sunil/Desktop/ytAPI/footballTwo.html", "r")
+# content = f.read()
+content = page.content
+print("_______________")
 
-f = open("footballTwo.html", "r")
-content = f.read()
 soup = BeautifulSoup(content, 'lxml')
 # print(soup.prettify())
 mylis = soup.find_all("li", {"class": "FootballLiveCompetitionList__CompetitionListItem-sc-112crmw-1 dRylsD"})
 # print(len(mylis))
 
+class LeagueObject:
+    def __init__(self, league_name, match_list):
+        self.league_name = league_name
+        self.match_list = match_list
+        self.date = splittedDate[0]
+
+    def __str__(self):
+        return ' League: ' + self.league_name + '\n matches: \t'+ str(self.match_list) + ' '+str(self.date)
+
+
+
+league_objects_list = []
+
 for lis in mylis:
     # company_names = lis.find_all('span', {"class": "CompactText__DataCompactText-sc-1rtlz3k-0 jdvCWg"})
     # print(lis)
     league_title = lis.find('h3', {"class": "ContentPanel__ContentPanelTitle-sc-1izwmji-1"})
-    print(league_title.text)
+    # print(league_title.text)
     company_names_first = lis.find_all('span', {"class": "FootballMatchListItem__TeamA-sc-9lgblw-6 khEMhE"})
     company_names_second = lis.find_all('span', {"class": "FootballMatchListItem__TeamB-sc-9lgblw-8 kVnMZR"})
 
     # print(len(company_names_first))
     # print(len(company_names_second))
+    match_list = []
     for i in range(0, len(company_names_first)):
-        print(company_names_first[i].text, ' vs ', company_names_second[i].text)
+        match_list.append(company_names_first[i].text + ' vs '+ company_names_second[i].text)
+    league_object = LeagueObject(league_title.text, match_list)
+
+    print(league_object)
+    league_objects_list.append(league_object)
     print("+++++++++++++++")
+
+print('000000000000')
+# print(league_objects_list)
